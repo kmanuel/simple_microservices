74af8799478fa9a7833be0c9d56fb4e315c3793e
manuel <kruisz88@gmail.com>
2018-12-10 19:55:35 +0100

    connnect most_significant_image_service via minioconnector

---
+++ self_implemented/src/service/most_significant_image/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/advancedlogic/GoOse"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"simple_microservices/self_implemented/src/minioconnector"
+)
+
+const OutputImageLocation = "/tmp/"
+
+type Request struct {
+	In     string `json:"in,omitempty"`
+	Out    string `json:"out,omitempty"`
+}
+
+func main() {
+	minioconnector.Init()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", HandleRequest).Methods("POST")
+	log.Println(http.ListenAndServe(":8086", router))
+}
+
+func HandleRequest(w http.ResponseWriter, r *http.Request) {
+	var task Request
+	_ = json.NewDecoder(r.Body).Decode(&task)
+	inputUrl := task.In
+	outputFile := OutputImageLocation + uuid.New().String() + ".jpg"
+
+	ExtractMostSignificantImage(inputUrl, outputFile)
+
+	minioconnector.UploadFile(outputFile)
+}
+
+func ExtractMostSignificantImage(inputUrl string, outputFile string) {
+	g := goose.New()
+	article, _ := g.ExtractFromURL(inputUrl)
+	topImageUrl := article.TopImage
+	DownloadImage(topImageUrl, outputFile)
+}
+
+func DownloadImage(url string, outputFile string) error {
+	filepath := outputFile
+
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
