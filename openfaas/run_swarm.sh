eb1dfcb4994b3c53b6a6508bcd81c2c506f5553e
kmanuel <kruisz88@gmail.com>
2019-03-22 12:23:40 +0100

    start minio server from run script

--- openfaas/run_swarm.sh
+++ openfaas/run_swarm.sh
@@ -1,6 +1,16 @@
 cd faas
 sh deploy_stack.sh
 
+docker service create \
+    -d \
+    --mount type=bind,source="$(pwd)"/imgs,target=/data \
+    -p "9000:9000" \
+    --network=func_functions \
+    --name minio \
+    --env-file ".env" \
+    minio/minio:RELEASE.2018-12-06T01-27-43Z \
+    server /data
+
 cd ../crop
 faas-cli build -f crop.yml
 faas-cli deploy -f crop.yml
