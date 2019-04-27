37b64bdb8f6a8f012388fbba48b43673e9e6c3db
Manuel Kruisz <kruisz88@gmail.com>
2019-04-27 12:54:32 +0200

    imrpove performance tests scripts

--- self_implemented/scripts/performance_test.py
+++ self_implemented/scripts/performance_test.py
@@ -1,55 +1,106 @@
+import os
 import requests
 import time
 import bs4
 import sys
-
+import time
+import subprocess
 
 verbose = False
-request_count = int(sys.argv[1])
 
-data = """{
-    "data": {
-        "type": "crop_task",
-        "attributes": {
-            "image_id": "surfcat.jpg",
-            "width": 50,
-            "height": 10
+def create_tasks(request_count):
+    data = """{
+        "data": {
+            "type": "crop_task",
+            "attributes": {
+                "image_id": "surfcat.jpg",
+                "width": 50,
+                "height": 10
+            }
         }
-    }
-}"""
+    }"""
 
-if verbose:
-    print('creating ' + str(request_count) + ' tasks')
-for i in range(0, request_count):
-    response = requests.post('http://127.0.0.1:8080/crop', data=data)
-    if response.status_code != 201:
-        print('received unexpected statuscode when creating task=' + str(response.status_code))
+    for i in range(0, request_count):
+        response = requests.post('http://127.0.0.1:8080/crop', data=data)
+        if response.status_code != 201:
+            print('received unexpected statuscode when creating task=' + str(response.status_code))
 
-if verbose:
-    print('created tasks')
 
-all_tasks_finished = False
+def time_till_all_tasks_done(request_count):
+    start = time.time()
+    while not is_all_tasks_finished():
+        time.sleep(0.25)
+    return time.time() - start
 
-start = time.time()
 
-while not all_tasks_finished:
-        all_tasks_finished = True
-        response = requests.get('http://127.0.0.1:7420/queues')
-        soup = bs4.BeautifulSoup(response.content, 'html.parser')
-        task_rows = soup.select('table.queues tr') #> td:nth-child(2)
-        for row in task_rows:
-            tds = row.select('td')
-            task_type = tds[0].text.strip()
-            number_text = tds[1].text.replace(',', '')
-            number_of_tasks_pending = int(number_text.strip())
-            if number_of_tasks_pending > 0:
-                all_tasks_finished = False
-                time.sleep(0.25)
+def is_all_tasks_finished():
+    return get_busy_task_count() == 0 and get_queue_task_count() == 0
 
-end = time.time()
+def get_busy_task_count():
+    response = requests.get('http://127.0.0.1:7420/queues')
+    soup = bs4.BeautifulSoup(response.content, 'html.parser')
+    task_count = soup.select('li.busy.col-sm-1 > a > span.count')
+    return int(task_count[0].text.strip())
 
-if verbose:
-    print('duration: ' + str(end - start))
-else:
-    print(str(end- start))
+def get_queue_task_count():
+    response = requests.get('http://127.0.0.1:7420/queues')
+    soup = bs4.BeautifulSoup(response.content, 'html.parser')
+    task_count = soup.select('li.enqueued.col-sm-1 > a > span.count')
+    return int(task_count[0].text.strip())
 
+
+
+def deploy_stack(with_resource_constraints):
+    os.system('docker stack rm self_impl_swarm > /dev/null')
+
+    if with_resource_constraints:
+        compose_file = "scripts/service-swarm-compose.yml "
+    else:
+        compose_file = "scripts/res-limit-service-swarm-compose.yml"
+
+    res = -1
+    while res != 0:
+        res = os.system('docker stack deploy --compose-file scripts/infrastructure-swarm-compose.yml --compose-file ' + compose_file + ' self_impl_swarm > /dev/null')
+    all_ready = False
+
+    while not all_ready:
+        time.sleep(0.25)
+        all_ready = True
+        replicas = subprocess.check_output("docker service ls | tr -s ' ' | cut -d' ' -f4", shell=True);
+        replicas_split = replicas.decode('utf-8').split('\n')
+        for replica in replicas_split:
+            if replica.startswith('0'):
+                all_ready = False
+
+
+
+
+def clear_img_dir():
+    os.system('rm -rf ./imgs/crop > /dev/null 2>&1 &')
+
+def setup_img_dir():
+    os.system('mkdir ./imgs/crop > /dev/null 2>&1 &')
+
+
+def perform_run(request_count,
+                with_resource_constraints,
+                with_auto_scale):
+    clear_img_dir()
+    setup_img_dir()
+    deploy_stack(with_resource_constraints=with_resource_constraints)
+    create_tasks(request_count)
+    processing_time = time_till_all_tasks_done(request_count)
+    print(str(request_count) + ";" + str(with_resource_constraints) + ";" + str(with_auto_scale) + ';' + str(processing_time))
+
+
+
+
+number_of_runs = 10
+request_counts = [1, 10, 100, 250]
+
+for i in range(0, number_of_runs):
+    for count in request_counts:
+        perform_run(request_count=count, with_resource_constraints=False, with_auto_scale=False)
+        perform_run(request_count=count, with_resource_constraints=False, with_auto_scale=True)
+        perform_run(request_count=count, with_resource_constraints=True, with_auto_scale=False)
+        perform_run(request_count=count, with_resource_constraints=True, with_auto_scale=True)
